0dc1c954503b6426fbf37a5f8ed07c6d3f414c35
Kyle Stankovich <gamemakergml@gmail.com>
2021-06-20 08:54:10 -0400

    Removed CoreModule module.

--- projects/stankng-demo/src/app/app.module.ts
+++ projects/stankng-demo/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { CoreModule } from './core/core.module';
 import { FootersModule } from './features/footers';
 import { HeadersModule } from './features/headers';
 import { LayoutsModule } from './features/layouts';
@@ -14,7 +13,6 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    CoreModule,
     FootersModule,
     HeadersModule,
     LayoutsModule
